479c266afcefd5572542ac8dd6e09f0b1086dd86
Pedestria <alex@topperfamily.org>
2021-04-03 11:40:46 -0400

    framework code sign --success

--- gn/codesign.py
+++ gn/codesign.py
@@ -1,16 +1,21 @@
 import argparse
 import os
 import shutil
+import sys
+import AppleFramework
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--sig",type=str)
 parser.add_argument("--code",type=str)
 parser.add_argument("--output_dir",type=str)
+parser.add_argument("--framework",dest="framework",action="store_const",const=True,default=False)
+args = parser.parse_known_args()
 
-args = parser.parse_args()
+if(args[0].framework):
+    AppleFramework.main(args[1])
 
-os.system("codesign --force  -s " + args.sig + " --verbose=3 " + args.code)
+os.system("codesign --force  -s " + args[0].sig + " --verbose=3 " + args[0].code)
 # output_file = os.path.join(args.output_dir,os.path.basename(args.code))
 # if(output_file):
 #     os.remove(output_file)
