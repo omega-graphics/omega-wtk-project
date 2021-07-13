b843c3dec166f4393f8f70aecfc663988e8b0267
Pedestria <42527102+Pedestria@users.noreply.github.com>
2021-07-13 15:39:34 -0400

    app build windows success + run test success

--- tests/RootWidget/Main.cpp
+++ tests/RootWidget/Main.cpp
@@ -4,7 +4,7 @@ using namespace OmegaWTK;
 
 int omegaWTKMain(OmegaWTK::AppInst *app){
 
-    auto window = make<AppWindow>(Core::Rect {500,500});
+    auto window = make<AppWindow>(Core::Rect {1000,1000});
 
     app->windowManager->setRootWindow(window);
     app->windowManager->displayRootWindow();
